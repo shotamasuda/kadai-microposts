48b9ce1e736bd9e575ebffb1b6375fdf0cf6ee7b
shota.masuda <pooxx823@yahoo.co.jp>
2019-07-25 02:59:56 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+    # id の降順にユーザの一覧を取得しています。ページネーションも付与
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
